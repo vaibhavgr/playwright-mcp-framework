c78bd4be77a782feb98db7f67f007a305f316978
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-05-27 13:08:37 +0530

    Refactor Code

--- pageObjects/loginPage.ts
+++ pageObjects/loginPage.ts
@@ -1,42 +1,43 @@
-import { Page } from '@playwright/test';
+import { Locator, Page, test  } from '@playwright/test';
 import { BasePage } from './basePage';
 
 export class LoginPage extends BasePage {
 
-    readonly loginEmail;
-    readonly loginPassword;
-    readonly loginBtn
+    readonly loginEmail: Locator;
+    readonly loginPassword: Locator;
+    readonly loginBtn: Locator;
 
     constructor(page: Page) {
         super(page);
-
         this.loginEmail = page.locator('[data-qa="login-email"]');
         this.loginPassword = page.locator('[data-qa="login-password"]')
         this.loginBtn = page.locator('[data-qa="login-button"]')
     }
 
     //----Actions-----
-    async goto() {
-        await this.navigateTo('/login');
+    async goto(): Promise<void> {
+        await test.step('Navigate to Login page', async () => {
+            await this.navigateTo('/login');
+        });
     }
-
-    async loginValidUser(email : string , password : string){
-        await  this.loginEmail.fill(email)
-        await this.loginPassword.fill(password)
-       
+    async loginValidUser(email: string, password: string): Promise<void> {
+        await test.step(`Enter valid credentials for ${email}`, async () => {
+            await this.loginEmail.fill(email);
+            await this.loginPassword.fill(password);
+        });
     }
-    
-    async loginBtnClick()
-    {
-       await this.loginBtn.click()
+    async loginBtnClick(): Promise<void> {
+        await test.step('Click Login Button', async () => {
+            await this.loginBtn.click();
+        });
     }
-
-    async logininvalidUser(email : string , password : string){
-        await this.loginEmail.fill(email)
-        await this.loginPassword.fill(password)
+    async logininvalidUser(email: string, password: string): Promise<void> {
+        await test.step(`Enter invalid credentials for ${email}`, async () => {
+            await this.loginEmail.fill(email);
+            await this.loginPassword.fill(password);
+        });
     }
-
-    //Assertions 
-
-
 }
+
+
+
