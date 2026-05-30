9142dfbe73994c844a0c5f74136259bdf02a5e8d
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-05-30 13:26:37 +0530

    API UTILS structure for API Testing

--- utils/APIUtils.ts
+++ utils/APIUtils.ts
@@ -6,4 +6,29 @@ export class ApiUtils {
     constructor(apiContext: APIRequestContext) {
         this.apiContext = apiContext;
     }
+
+    /**
+     * Re-usable GET Request 
+     * @param endpoint Target API Path 
+     */
+
+    async get(endpoint: string): Promise<any> {
+        const response = await this.apiContext.get(endpoint);
+        const responseText = await response.text();
+        return JSON.parse(responseText)
+    }
+
+    /**
+     * Re-usable POST Request 
+     * @param endpoint Target API Path 
+     * @param payload Key-Value pairs to send in the request body
+     */
+
+    async postCall(endpoint: string, payload: Record<string, unknown>): Promise<any> {
+        const response = await this.apiContext.post(endpoint, {
+            data: payload
+        });
+        const responseText = await response.text();
+        return JSON.parse(responseText);
+    }
 }
