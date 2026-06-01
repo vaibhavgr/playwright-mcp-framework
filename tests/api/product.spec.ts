fd1abd29e3b44c5af80205c8075bb64f4afd782e
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-06-01 09:36:07 +0530

    API Integration V.01

---
+++ tests/api/product.spec.ts
@@ -0,0 +1,7 @@
+import { test } from '@fixtures/baseTest';
+
+test.describe('API Testing - Products using ApiUtils', () => {
+    test.only('Get - all product list', async ({ apiUtil }) => {
+        
+    })
+})
