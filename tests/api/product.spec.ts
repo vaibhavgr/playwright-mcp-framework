6a1e1a09a3a0aba04e8f7de1390ed75bf53a1018
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-06-02 21:48:22 +0530

    API Test Case 1

--- tests/api/product.spec.ts
+++ tests/api/product.spec.ts
@@ -1,7 +1,13 @@
 import { test } from '@fixtures/baseTest';
+import { expect } from '@playwright/test';
 
 test.describe('API Testing - Products using ApiUtils', () => {
     test.only('Get - all product list', async ({ apiUtil }) => {
+        const response = await apiUtil.get('/api/productsList');
+        expect(response.responseCode).toBe(200);
+        expect(response.products).toBeDefined();
+        expect(Array.isArray(response.products)).toBe(true);
+        expect(response.products.length).toBeGreaterThan(0);
         
     })
 })
