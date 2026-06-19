84facfaeaa776cd3fe97f8c4a80bc1026780ac9b
Vaibhav Grover <grovervaibhav17@gmail.com>
2026-06-19 23:16:24 +0530

    Network Interception API

---
+++ tests/network/networkmocking.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '@fixtures/baseTest';
+import { expect } from '@playwright/test';
+import { Logger } from '@utils/Logger';
+
+test.describe('Advanced Network Interception & Observability',()=>{
+ test('TC_NET_01: Mock API Interception - Mock Brand List', async ({ page }) => {
+        
+    const mockBrands = [];
+          });
+});
